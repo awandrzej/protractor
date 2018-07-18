189347291ec923f93942cc97108b4a14af6553d3
andrzej.wudara <andrzej.wudara@dsv.com>
2018-07-18 16:14:01 +0200

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require ('gulp'); 
+var protractor = require ('gulp-protractor').protractor; 
+var webdriver_update = require('gulp-protractor').webdriver_update; 
+var webdriver_standalone = require('gulp-protractor').webdriver_standalone; 
+
+gulp.task('webdriver_update', webdriver_update); 
+gulp.task('webdriver_standalone', webdriver_standalone); 
+
+gulp.task('default', ['webdriver_update', 'webdriver_standalone'],function(){
+
+    gulp.src(['todo-spec.js'])
+    .pipe (protractor({
+        configFile: "conf.js", 
+        args: ['--baseUrl', 'http://127.0.0.1:8000']
+
+    }))
+.on('error', function(e){throw e}); 
+}); 
