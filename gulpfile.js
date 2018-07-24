b7fbdeffa53206cc260fdfff07041bf8039fe734
awandrzej <andrzej.wudara@gmail.com>
2018-07-24 20:41:18 +0200

    gulp param

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
 var gulp = require ('gulp'); 
 var protractor = require ('gulp-protractor').protractor; 
 var webdriver_update = require('gulp-protractor').webdriver_update; 
-var webdriver_standalone = require('gulp-protractor').webdriver_standalone; 
-
 gulp.task('webdriver_update', webdriver_update); 
-gulp.task('webdriver_standalone', webdriver_standalone); 
 
-gulp.task('default', ['webdriver_update', 'webdriver_standalone'],function(){
+
+/*
+var webdriver_standalone = require('gulp-protractor').webdriver_standalone; 
+gulp.task('webdriver_standalone', webdriver_standalone); 
+*/
+
+gulp.task('default', function() {
 
     gulp.src(['todo-spec.js'])
     .pipe (protractor({
@@ -14,5 +17,7 @@ gulp.task('default', ['webdriver_update', 'webdriver_standalone'],function(){
         args: ['--baseUrl', 'http://127.0.0.1:8000']
 
     }))
-.on('error', function(e){throw e}); 
+    .on('error', function(e){throw e}); 
+
+
 }); 
--- todo-spec.js
+++ todo-spec.js
@@ -9,16 +9,17 @@ describe('angularjs homepage todo list', function() {
     }
     
     it('take a screenshot from KLM', function() {
-      
+    console.log('starting.....');   
     browser.get(browser.params.carrier.url+browser.params.carrier.master);
       
     element(by.id('set_notification')).getLocation(); 
     element(by.css('footer > div:nth-child(2) > hr')).getLocation().then(function (location) {
         var height = location.y; 
-        console.log('element size:'+height); 
+        console.log('element position: '+height); 
         browser.driver.manage().window().setSize(800, height);
         browser.takeScreenshot().then(function (png) {
-            writeScreenShot(png, 'exception.png');
+            writeScreenShot(png, browser.params.carrier.master+'.png');
+            console.log("screnshot:" + browser.params.carrier.master)
             });
         
     });
